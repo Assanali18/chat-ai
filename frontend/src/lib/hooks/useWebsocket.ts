3fdb83fd59e450043bf6499828c18537b3098f27
Assanali18 <ustaevasanali@gmail.com>
2024-06-24 12:04:45 +0500

    fix sockets

--- frontend/src/lib/hooks/useWebsocket.ts
+++ frontend/src/lib/hooks/useWebsocket.ts
@@ -12,7 +12,9 @@ const useWebSocket = (url: string) => {
 
   useEffect(() => {
     const connect = () => {
-      const webSocket = new WebSocket(url);
+      const protocol = window.location.protocol === 'https:' ? 'wss' : 'ws';
+      const webSocketUrl = url.replace(/^ws:/, `${protocol}:`);
+      const webSocket = new WebSocket(webSocketUrl);
       webSocketRef.current = webSocket;
 
       webSocket.onopen = () => {
